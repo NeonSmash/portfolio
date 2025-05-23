f80f6412f768eb6ec6970fe8484c569d7e742d02
NeonSmash <beleczky.feri@gmail.com>
2025-05-23 17:56:32 +0200

    Refining

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -13,17 +13,10 @@ export class FooterComponent {
   currentYear = new Date().getFullYear();
   
   socialLinks = [
-<<<<<<< HEAD
     { name: 'Facebook', url: 'https://www.facebook.com/beleczky.ferenc', icon: 'fab fa-facebook' },
     { name: 'Github', url: 'https://github.com/NeonSmash', icon: 'fab fa-github' },
     { name: 'Instagram', url: 'https://www.instagram.com/extravagans_gepsarkanyok/', icon: 'fab fa-instagram' },
     { name: 'LinkedIn', url: 'https://www.linkedin.com/in/beleczky-ferenc/', icon: 'fab fa-linkedin' }
-=======
-    { name: 'Facebook', url: '#', icon: 'fab fa-facebook' },
-    { name: 'Github', url: '#', icon: 'fab fa-github' },
-    { name: 'Instagram', url: '#', icon: 'fab fa-instagram' },
-    { name: 'LinkedIn', url: '#', icon: 'fab fa-linkedin' }
->>>>>>> cc37636161d3a6d1a63f11f75b9ad7fbae13b58c
   ];
 
   footerLinks = [
